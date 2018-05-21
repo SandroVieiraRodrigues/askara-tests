25e9f09c8ad531894f16e64b2ac3029903477a4f
Sandro Vieira Rodrigues <sandrooovr@gmail.com>
2018-05-21 00:59:46 -0300

    Commit de Schema Askara: 1526875186556

--- localhost/trabalho_alisson_2/TABLE/x1sspersons.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `x1sspersons` (
-  `PersonID` int(11) DEFAULT NULL,
-  `LastName` varchar(255) DEFAULT NULL,
-  `FirstName` varchar(255) DEFAULT NULL,
-  `Address` varchar(255) DEFAULT NULL,
-  `City` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
