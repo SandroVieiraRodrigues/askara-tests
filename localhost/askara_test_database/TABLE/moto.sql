7d5eb0982aaacc4afa79a71dce83b8f6d6dfc609
SandroVieiraRodrigues <sandrooovr@gmail.com>
2018-06-26 15:46:45 -0300

    [Askara], feito por "Rodrigo" na Máquina: "DESKTOP-0MEBC2B" ALTER TABLE moto drop column cor;

--- localhost/askara_test_database/TABLE/moto.sql
+++ localhost/askara_test_database/TABLE/moto.sql
@@ -2,6 +2,5 @@ CREATE TABLE `moto` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) DEFAULT NULL,
   `marca` varchar(50) DEFAULT NULL,
-  `cor` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
